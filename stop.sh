34940f438ce267e84045e932c9f46e9fa6dcf0ba
Daniel <dpzafra70@gmail.com>
2024-11-13 19:07:06 +0100

    end

--- stop.sh
+++ stop.sh
@@ -1,10 +1,12 @@
 docker-compose stop
 docker-compose rm
 #remoce containers
-docker rm $(docker ps -a -f status=exited -q)
+docker rm $(docker ps --all -q)
+#docker rm $(docker ps -a -f status=exited -q)
 #remove persistent data
 rm -r ./mysql/data
 #delete images
-docker rmi $(docker images -a -q)
+docker rmi --force $(docker images -a -q)
+#docker --force rmi $(docker images -a -q)
 #Delete volumen
 docker volume prune
